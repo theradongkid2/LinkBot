e8550d5fe6be1cbc84e7314e0ef35e67ae515022
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-01-12 13:42:16 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 const token = process.env.token;
-const sbID = "";
-const sgID = "";
+//const sbID = "";
+//const sgID = "";
 const ruseID = "";
 const chsID = "";
 
@@ -19,8 +19,7 @@ client.on('shardError', error => {
 //Terminal User Interface
 client.on("ready", () => {
   console.log(`Link has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`(-l) Linking ${client.guilds.size} Servers`);
-
+  client.user.setActivity(`Linking Schools`);
    // List servers the bot is connected to
    console.log("Servers:");
    client.guilds.forEach((guild) => {
@@ -35,27 +34,28 @@ client.on('message', message => {
   let guild = message.guild;
   let member = guild.member(message.author);
   let nickname = member ? member.displayName : null;
+  if(message.content.startsWith("-l")) return;
   if(message.channel.id === ruseID){
       var messageContent = message.content;
-      client.channels.get(sbID).send(`${nickname} (JRAHS): ${messageContent}`);
-      client.channels.get(sgID).send(`${nickname} (JRAHS): ${messageContent}`);
-      client.channels.get(chsID).send(`${nickname} (JRAHS): ${messageContent}`);
-  } else if(message.channel.id === sbID){
+      //client.channels.get(sbID).send(`${nickname} (JRAHS): ${messageContent}`);
+      //client.channels.get(sgID).send(`${nickname} (JRAHS): ${messageContent}`);
+      client.channels.get(chsID).send(`${nickname}: ${messageContent}`);
+  }/* else if(message.channel.id === sbID){
     var messageContent = message.content;
     client.channels.get(sgID).send(`${nickname} (JRAHS): ${messageContent}`);
     client.channels.get(chsID).send(`${nickname} (JRAHS): ${messageContent}`);
     client.channels.get(ruseID).send(`${nickname} (JRAHS): ${messageContent}`);
-  } else if(message.channel.id === chsID){
+  } */else if(message.channel.id === chsID){
     var messageContent = message.content;
-    client.channels.get(sgID).send(`${nickname} (JRAHS): ${messageContent}`);
-    client.channels.get(sbID).send(`${nickname} (JRAHS): ${messageContent}`);
-    client.channels.get(ruseID).send(`${nickname} (JRAHS): ${messageContent}`);
-  } else if(message.channel.id === sgID){
+    //client.channels.get(sgID).send(`${nickname} (JRAHS): ${messageContent}`);
+    //client.channels.get(sbID).send(`${nickname} (JRAHS): ${messageContent}`);
+    client.channels.get(ruseID).send(`${nickname}: ${messageContent}`);
+  }/* else if(message.channel.id === sgID){
     var messageContent = message.content;
     client.channels.get(sbID).send(`${nickname} (JRAHS): ${messageContent}`);
     client.channels.get(chsID).send(`${nickname} (JRAHS): ${messageContent}`);
     client.channels.get(ruseID).send(`${nickname} (JRAHS): ${messageContent}`);
-  }
+  }*/
 });
 
 client.on("message", async message => {
@@ -63,38 +63,15 @@ client.on("message", async message => {
     if(message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-
-    if(command === "invite") {
-        const inviteEmbed = {
-            color: 0x9932CC,
-            title: 'LinkBot Invite Link',
-            description: 'https://discord.com/api/oauth2/authorize?client_id=730004103719288904&permissions=2147483639&scope=bot',
-            thumbnail: {
-                url: 'https://cdn.discordapp.com/avatars/730004103719288904/ce269b42ef41f924bdeb4e3de9d0cb26.png?size=2048',
-            },
-            
-            timestamp: new Date(),
-            footer: {
-                text: 'LinkBot by Joshua Koh',
-                icon_url: 'https://cdn.discordapp.com/avatars/730004103719288904/ce269b42ef41f924bdeb4e3de9d0cb26.png?size=2048',
-            },
-        };
-        
-        message.channel.send({ embed: inviteEmbed });
-        
-      }
-    
     if(command === "say") {
         const sayMessage = args.join(" ");
         message.delete().catch(O_o=>{}); 
         message.channel.send(`${sayMessage}`);
     }
-    
     if(command === "ping") {
         const m = await message.channel.send("Ping!");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-      }
-    
+    }
 });
 
 //Client Login
